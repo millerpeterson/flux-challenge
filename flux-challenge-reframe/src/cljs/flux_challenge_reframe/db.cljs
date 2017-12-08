caf316839ca8802bde250a0e3e0703c2ede8de7c
Miller Peterson <miller.peterson@gmail.com>
2017-12-07 23:32:41 -0500

    Default DB

--- flux-challenge-reframe/src/cljs/flux_challenge_reframe/db.cljs
+++ flux-challenge-reframe/src/cljs/flux_challenge_reframe/db.cljs
@@ -1,4 +1,9 @@
 (ns flux-challenge-reframe.db)
 
 (def default-db
-  {:name "re-frame"})
+  {:obi-wan {:location "Crematoria"}
+   :sith [{:name "Billy Coolguy"
+           :homeworld "Earth"}
+          {:name "Wolly Woodberg"
+           :homeworld "The Crab Nebula"}]
+   :scroll-pos 0})
